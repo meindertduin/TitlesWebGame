d8fdc2c7f3786b6bafc58c8e4c992bd2e4c76578
Meindert <meindertduin@hotmail.com>
2021-01-06 11:44:32 +0100

    added repository pattern

---
+++ src/TitlesWebGame.Api/Infrastructure/Repositories/GameRoundInfoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using TitlesWebGame.Domain.Entities;
+
+namespace TitlesWebGame.Api.Infrastructure.Repositories
+{
+    public class GameRoundInfoRepository : Repository<GameRoundInfo>
+    {
+        public GameRoundInfoRepository(string tableName, IConfiguration configuration) : base(tableName, configuration)
+        {
+            
+        }
+        
+    }
+}
---
+++ src/TitlesWebGame.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TitlesWebGame.Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task DeleteRowAsync(int id);
+        Task<T> GetAsync(int id);
+        Task<int> SaveRangeAsync(IEnumerable<T> list);
+        Task UpdateAsync(T t);
+        Task InsertAsync(T t);
+    }
+}
