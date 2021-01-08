6b238b750c3b0c7dafd1a26675ed45c165db4e69
Meindert <meindertduin@hotmail.com>
2021-01-08 11:19:55 +0100

    added synfusion for datagrid

--- src/TitlesWebGame.WebUi/Program.cs
+++ src/TitlesWebGame.WebUi/Program.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Syncfusion.Blazor;
 using TitlesWebGame.WebUi.Services;
 using TitlesWebGame.WebUi.ViewModels;
 
@@ -15,11 +16,13 @@ namespace TitlesWebGame.WebUi
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mzc5OTA5QDMxMzgyZTM0MmUzMGdCVmR0Z091emxIMVo0SFprUmorS2Q2WmU4Y2hvR0RlSEFOVVlhLzNkSlU9"); 
             builder.Services.AddScoped<GameSocketConnectionManager>();
             builder.Services.AddScoped<GameViewModel>();
             builder.Services.AddTransient<GameSocketServerMessageHandler>();
             builder.Services.AddSingleton<GameSessionState>();
             builder.Services.AddTransient<ITitlesCategoryEmojiFactory, TitlesCategoryEmojiFactory>();
+            builder.Services.AddSyncfusionBlazor();
 
             builder.Services.AddSingleton<ApplicationViewModel>();
 
