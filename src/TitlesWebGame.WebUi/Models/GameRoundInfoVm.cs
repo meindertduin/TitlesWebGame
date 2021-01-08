ec1c7943dbe89c5ded17f6ee4600dc28d666800b
Meindert <meindertduin@hotmail.com>
2021-01-08 11:44:47 +0100

    basic datagrid for display gamerounds

---
+++ src/TitlesWebGame.WebUi/Models/GameRoundInfoVm.cs
@@ -0,0 +1,18 @@
+﻿using TitlesWebGame.Domain.Enums;
+
+namespace TitlesWebGame.WebUi.Models
+{
+    public class GameRoundInfoVm
+    {
+        public int Id { get; set; }
+        public GameRoundsType GameRoundsType { get; set; }
+        public TitleCategory TitleCategory { get; set; }
+        public int RewardPoints { get; set; }
+        public int RoundTimeMs { get; set; }
+        public int PaintingRoundTimeMs { get; set; }
+        public int VotingRoundTimeMs { get; set; }
+        public string RoundStatement { get; set; }
+        public string Answer { get; set; }
+        public string Choices { get; set; }
+    }
+}
